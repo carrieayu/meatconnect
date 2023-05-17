8f40d02d32e3552ea964e377c8dbce26d4c54b15
Carre Yu <carrie@bpoc.co.jp>
2023-05-17 11:58:25 +0800

    udpates

--- src/pages/AccntRecovery.jsx
+++ src/pages/AccntRecovery.jsx
@@ -6,16 +6,139 @@ import { useNavigate } from "react-router-dom";
 const AccntRecovery = () => {
   const navigate = useNavigate();
   const [recover, setRecover] = React.useState(false);
+  const [secQuest, setSecQuest] = React.useState(false);
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [confirmPass, setConfirmPass] = React.useState("");
+  const [questPass, setQuestPass] = React.useState(false);
+  const [questionOne, setQuestionOne] = React.useState();
+  const [questionOneId, setQuestionOneId] = React.useState();
+  const [questionTwo, setQuestionTwo] = React.useState();
+  const [questionTwoId, setQuestionTwoId] = React.useState();
+  const [questionThree, setQuestionThree] = React.useState();
+  const [questionThreeId, setQuestionThreeId] = React.useState();
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
+
+  const onSubmitAnswer = () => {
+    let oneCheck = false;
+    let twoCheck = false;
+    let threeCheck = false;
+
+    const checkAnswers = () => {
+      if (oneCheck && twoCheck && threeCheck) {
+        setSecQuest(false);
+        setQuestPass(true);
+      }
+    };
+
+    if (questionOneId) {
+      axios
+        .post(`http://localhost:8080/questions/checkAnswer/${questionOneId}`, {
+          answer: answerOne,
+        })
+        .then((res) => {
+          console.log(res.data.length);
+          if (res.data.length > 0) {
+            oneCheck = true;
+          }
+          checkAnswers();
+        })
+        .catch((err) => {
+          console.error(err);
+          alert("Error occurred while checking the answer. Please try again.");
+        });
+    }
+
+    if (questionTwoId) {
+      axios
+        .post(`http://localhost:8080/questions/checkAnswer/${questionTwoId}`, {
+          answer: answerTwo,
+        })
+        .then((res) => {
+          console.log(res.data.length);
+          if (res.data.length > 0) {
+            twoCheck = true;
+          }
+          checkAnswers();
+        })
+        .catch((err) => {
+          console.error(err);
+          alert("Error occurred while checking the answer. Please try again.");
+        });
+    }
+
+    if (questionThreeId) {
+      axios
+        .post(
+          `http://localhost:8080/questions/checkAnswer/${questionThreeId}`,
+          {
+            answer: answerThree,
+          }
+        )
+        .then((res) => {
+          console.log(res.data.length);
+          if (res.data.length > 0) {
+            threeCheck = true;
+          }
+          checkAnswers();
+        })
+        .catch((err) => {
+          console.error(err);
+          alert("Error occurred while checking the answer. Please try again.");
+        });
+    }
+  };
 
   const onSubmit = () => {
     if (email === "") {
-      alert("Email Field is required!");
+      alert("Email field is required!");
       return;
     }
-    setRecover(true);
+
+    axios
+      .get(`http://localhost:8080/user/retrieveEmail/${email}`)
+      .then((res) => {
+        if (res.data) {
+          axios
+            .get(
+              `http://localhost:8080/security_question/retrieveByUserWithQuestion/${res.data.user_id}`
+            )
+            .then((result) => {
+              if (result.data) {
+                setRecover(true);
+                setSecQuest(true);
+                result.data.map((d, index) => {
+                  if (d.question_number === 1) {
+                    setQuestionOne(d.question);
+                    setQuestionOneId(d.seucrity_id);
+                  } else if (d.question_number === 2) {
+                    setQuestionTwo(d.question);
+                    setQuestionTwoId(d.seucrity_id);
+                  } else if (d.question_number === 3) {
+                    setQuestionThree(d.question);
+                    setQuestionThreeId(d.seucrity_id);
+                  }
+                });
+              } else {
+                alert(
+                  "Email not found or user does not have security questions set."
+                );
+              }
+            })
+            .catch((err) => {
+              alert("Error retrieving security questions.");
+              console.error(err);
+            });
+        } else {
+          alert("Email not found.");
+        }
+      })
+      .catch((err) => {
+        alert("Error retrieving email.");
+        console.error(err);
+      });
   };
 
   const onResetPassword = () => {
@@ -46,7 +169,7 @@ const AccntRecovery = () => {
     <>
       <Navbar />
       <div className="container my-3 py-3">
-        <h1 className="text-center">Login</h1>
+        <h1 className="text-center">Recover Account</h1>
         <hr />
         <div class="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
@@ -69,7 +192,63 @@ const AccntRecovery = () => {
                   </button>
                 </div>
               </>
-            ) : (
+            ) : null}
+            {secQuest ? (
+              <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center experience">
+                  <span>Security Questions</span>
+                </div>
+                <br />
+                <div class="col-md-12">
+                  <span>{questionOne}</span>
+                </div>
+                <div class="col-md-12">
+                  <label class="labels">Answer One</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Answer One"
+                    value={answerOne}
+                    onChange={(event) => setAnswerOne(event.target.value)}
+                  />
+                </div>
+                <div class="col-md-12">
+                  <span>{questionTwo}</span>
+                </div>
+                <div class="col-md-12">
+                  <label class="labels">Answer One</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Answer Two"
+                    value={answerTwo}
+                    onChange={(event) => setAnswerTwo(event.target.value)}
+                  />
+                </div>
+                <div class="col-md-12">
+                  <span>{questionThree}</span>
+                </div>
+                <div class="col-md-12">
+                  <label class="labels">Answer One</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Answer Three"
+                    value={answerThree}
+                    onChange={(event) => setAnswerThree(event.target.value)}
+                  />
+                </div>
+                <div className="text-center">
+                  <button
+                    class="my-2 mx-auto btn btn-dark"
+                    onClick={onSubmitAnswer}
+                  >
+                    Submit Answer
+                  </button>
+                </div>
+              </div>
+            ) : null}
+            {questPass ? (
               <>
                 <div class="form  my-3">
                   <label for="Password">Password</label>
@@ -104,7 +283,7 @@ const AccntRecovery = () => {
                   </button>
                 </div>
               </>
-            )}
+            ) : null}
           </div>
         </div>
       </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,6 +18,16 @@ const Profile = () => {
   const [error, setError] = React.useState("");
   const [selectedFile, setSelectedFile] = React.useState(null);
   const [selectedFileUrl, setSelectedFileUrl] = React.useState("");
+  const [question, setQuestion] = React.useState([]);
+  const [questionOne, setQuestionOne] = React.useState();
+  const [questionOneId, setQuestionOneId] = React.useState();
+  const [questionTwo, setQuestionTwo] = React.useState();
+  const [questionTwoId, setQuestionTwoId] = React.useState();
+  const [questionThree, setQuestionThree] = React.useState();
+  const [questionThreeId, setQuestionThreeId] = React.useState();
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
 
   const onChangePass = (event) => {
     axios
@@ -59,6 +69,129 @@ const Profile = () => {
     event.preventDefault();
   };
 
+  const onSecuritySubmit = () => {
+    axios
+      .get(
+        `http://localhost:8080/security_question/retrieveByUser/${localStorage.getItem(
+          "id"
+        )}`
+      )
+      .then((res) => {
+        if (res.data.length) {
+          if (questionOne) {
+            axios
+              .put(
+                `http://localhost:8080/security_question/update/${questionOneId}`,
+                {
+                  answer: answerOne,
+                  question_id: questionOne,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+
+          if (questionTwo) {
+            axios
+              .put(
+                `http://localhost:8080/security_question/update/${questionTwoId}`,
+                {
+                  answer: answerTwo,
+                  question_id: questionTwo,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+
+          if (questionThree) {
+            axios
+              .put(
+                `http://localhost:8080/security_question/update/${questionThreeId}`,
+                {
+                  answer: answerThree,
+                  question_id: questionThree,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+        } else {
+          if (questionOne) {
+            axios
+              .post(
+                `http://localhost:8080/security_question/insert/${questionOne}/${localStorage.getItem(
+                  "id"
+                )}`,
+                {
+                  answer: answerOne,
+                  number: 1,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+
+          if (questionTwo) {
+            axios
+              .post(
+                `http://localhost:8080/security_question/insert/${questionTwo}/${localStorage.getItem(
+                  "id"
+                )}`,
+                {
+                  answer: answerTwo,
+                  number: 2,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+
+          if (questionThree) {
+            axios
+              .post(
+                `http://localhost:8080/security_question/insert/${questionThree}/${localStorage.getItem(
+                  "id"
+                )}`,
+                {
+                  answer: answerThree,
+                  number: 3,
+                }
+              )
+              .then((res) => {
+                console.log(res);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          }
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   const onSubmit = (event) => {
     if (newPass !== confirmPass) {
       setError("Password Does not match");
@@ -93,6 +226,47 @@ const Profile = () => {
       });
   };
 
+  const fetchQuestion = () => {
+    axios
+      .get(`http://localhost:8080/questions/retrieve`)
+      .then((res) => {
+        console.log(res.data);
+        setQuestion(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const fetchSecurityQuestion = () => {
+    axios
+      .get(
+        `http://localhost:8080/security_question/retrieveByUser/${localStorage.getItem(
+          "id"
+        )}`
+      )
+      .then((res) => {
+        res.data.map((d, index) => {
+          if (d.question_number === 1) {
+            setQuestionOne(d.question_id);
+            setAnswerOne(d.answer);
+            setQuestionOneId(d.id);
+          } else if (d.question_number === 2) {
+            setQuestionTwo(d.question_id);
+            setAnswerTwo(d.answer);
+            setQuestionTwoId(d.id);
+          } else if (d.question_number === 3) {
+            setQuestionThree(d.question_id);
+            setAnswerThree(d.answer);
+            setQuestionThreeId(d.id);
+          }
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   const fetchUser = () => {
     const user_id = localStorage.getItem("id");
 
@@ -121,6 +295,8 @@ const Profile = () => {
   };
 
   React.useEffect(() => {
+    fetchSecurityQuestion();
+    fetchQuestion();
     fetchUser();
   }, []);
 
@@ -253,6 +429,92 @@ const Profile = () => {
                   placeholder="Confirm Password"
                   onChange={(event) => setConfirmPass(event.target.value)}
                 />
+              </div>
+            </div>
+            <div class="p-3 py-5">
+              <div class="d-flex justify-content-between align-items-center experience">
+                <span>Update Security Questions</span>
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Question 1</label>
+                <select
+                  class="form-select"
+                  value={questionOne}
+                  onChange={(e) => setQuestionOne(e.target.value)}
+                  aria-label="Default select example"
+                >
+                  <option selected>Open this select menu</option>
+                  {question?.map((d) => (
+                    <option value={d.id}>{d.question}</option>
+                  ))}
+                </select>
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Answer One</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Answer One"
+                  value={answerOne}
+                  onChange={(event) => setAnswerOne(event.target.value)}
+                />
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Question 2</label>
+                <select
+                  class="form-select"
+                  value={questionTwo}
+                  onChange={(e) => setQuestionTwo(e.target.value)}
+                  aria-label="Default select example"
+                >
+                  <option selected>Open this select menu</option>
+                  {question?.map((d) => (
+                    <option value={d.id}>{d.question}</option>
+                  ))}
+                </select>
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Answer Two</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Answer One"
+                  value={answerTwo}
+                  onChange={(event) => setAnswerTwo(event.target.value)}
+                />
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Question 3</label>
+                <select
+                  class="form-select"
+                  value={questionThree}
+                  onChange={(e) => setQuestionThree(e.target.value)}
+                  aria-label="Default select example"
+                >
+                  <option selected>Open this select menu</option>
+                  {question?.map((d) => (
+                    <option value={d.id}>{d.question}</option>
+                  ))}
+                </select>
+              </div>
+              <div class="col-md-12">
+                <label class="labels">Answer Three</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Answer One"
+                  value={answerThree}
+                  onChange={(event) => setAnswerThree(event.target.value)}
+                />
+              </div>
+              <div class="col-md-12 mt-5 text-center">
+                <button
+                  class="btn btn-primary profile-button mr-4"
+                  type="button"
+                  onClick={onSecuritySubmit}
+                >
+                  Submit Security Question
+                </button>
               </div>
             </div>
           </div>
